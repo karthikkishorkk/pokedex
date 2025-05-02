5066b83b2ffbf34849885b9b7b911caf2fec22f5
karthikkishorkk <karthikkishor.4c@gmail.com>
2025-05-02 10:58:06 +0530

    Path Updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
-import Details from "./pages/Details";
+import Home from "./pages/Home/Home";
+import Details from "./pages/Details/Details";
 
 const App = () => {
   return (
